d83da92031d8d9bc7e48fc0e3240f4a3bfccf402
peter staal <peterstaal@MBP-van-peter.home>
2024-03-15 09:30:29 +0100

    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,39 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createStore, combineReducers } from "redux";
+import noteReducer from "./reducers/noteReducer";
+import filterReducer from "./reducers/filterReducer";
+import Notes from "./components/Notes";
+import NewNote from "./components/NewNote";
+import VisibilityFilter from "./components/VisibilityFilter";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+const App = () => {
+  return (
+    <div>
+      <NewNote />
+      <VisibilityFilter />
+      <Notes />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+renderApp();
+store.subscribe(renderApp);
--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,4 +1,17 @@
-const noteReducer = (state = [], action) => {
+const initialState = [
+  {
+    content: "reducer defines how redux store works",
+    important: true,
+    id: 1,
+  },
+  {
+    content: "state of store can contain any data",
+    important: false,
+    id: 2,
+  },
+];
+
+const noteReducer = (state = initialState, action) => {
   switch (action.type) {
     case "NEW_NOTE":
       return [...state, action.payload];
@@ -14,6 +27,26 @@ const noteReducer = (state = [], action) => {
     default:
       return state;
   }
+};
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+export const createNote = (content) => {
+  return {
+    type: "NEW_NOTE",
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: { id },
+  };
 };
 
 export default noteReducer;
